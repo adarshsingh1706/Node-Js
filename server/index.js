2538dd056e0d01b5db9ea046c524982773ff841c
Adarsh Singh <adarsh.singh2022@vitstudent.ac.in>
2024-03-09 18:12:35 +0530

    handling url

--- server/index.js
+++ server/index.js
@@ -1,19 +1,28 @@
 const { log } = require("console");
 const http = require("http");
 const fs = require("fs");
+const url = require("url");
 
 
 const myServer = http.createServer((req, res) => {
   // this fn is responsible for processing incoming req
   const log = `${Date.now()}: ${req.url} New Request Received\n`;
+  const myurl = url.parse(req.url,true); // true for parsing query
+  console.log(myurl);
   fs.appendFile("log.txt", log, (err, data) => {
-    switch (req.url) {
+    switch (myurl.pathname) {
       case "/":
         res.end("Homepage");
         break;
       case "/about":
-        res.end("I am Adarsh Singh");
+        const username = myurl.query.myName;
+        res.end(`Hi ${username}`);
         break;
+
+        case "/search":
+          const search = myurl.query.search_query;
+          res.end(`these r the results for ${search}` );
+          break;
       default:
         res.end("404 NOt Found");
     }
@@ -21,3 +30,4 @@ const myServer = http.createServer((req, res) => {
 });
 
 myServer.listen(8000, () => console.log("Server Started!"));
+ 

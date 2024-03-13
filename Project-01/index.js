9206ef2e34b39d65eb2fa3889f156b1168a103ae
Adarsh Singh <adarsh.singh2022@vitstudent.ac.in>
2024-03-13 21:27:43 +0530

    rest api

---
+++ Project-01/index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const app = express();
+const users = require('./MOCK_DATA.json')
+const PORT =8000;
+
+//Routes
+
+//for html rendering
+app.get("/users",(req,res)=>{
+  const html =`
+  <ul>
+  ${users.map((e) =>`<li>${e.first_name}</li>`).join("")}
+  </ul>
+  `
+  res.send(html);
+})
+
+
+//REST api
+
+app.get("/api/users",(req,res)=>{
+  return res.json(users);
+})
+
+app.get("/api/users/:id", (req,res)=>{
+  const id = Number(req.params.id); // finding id req se then compairing
+  const user = users.find((user)=>user.id===id); // comparing from DB
+  return res.json(user);
+})
+
+
+
+
+
+
+
+
+
+app.listen(PORT,()=>console.log(`Server Started at Port: ${PORT}`));

b293f4a58697a9aef791f7f7acc2f5152cfa37fc
Adarsh Singh <adarsh.singh2022@vitstudent.ac.in>
2024-03-14 12:18:18 +0530

    patch&delete added

--- Project-01/index.js
+++ Project-01/index.js
@@ -1,31 +1,86 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const users = require('./MOCK_DATA.json')
-const PORT =8000;
+const fs = require("fs");
+const users = require("./MOCK_DATA.json");
+const PORT = 8000;
+
+//middleware:plugin
+app.use(express.urlencoded({ extended: false }));
 
 //Routes
 
 //for html rendering
-app.get("/users",(req,res)=>{
-  const html =`
+app.get("/users", (req, res) => {
+  const html = `
   <ul>
-  ${users.map((e) =>`<li>${e.first_name}</li>`).join("")}
+  ${users.map((e) => `<li>${e.first_name}</li>`).join("")}
   </ul>
-  `
+  `;
   res.send(html);
-})
-
+});
 
 //REST api
 
-app.get("/api/users",(req,res)=>{
+app.get("/api/users", (req, res) => {
   return res.json(users);
-})
+});
 
-app.get("/api/users/:id", (req,res)=>{
+//to get info
+app.get("/api/users/:id", (req, res) => {
   const id = Number(req.params.id); // finding id req se then compairing
-  const user = users.find((e)=>e.id===id); // comparing from DB
+  const user = users.find((e) => e.id === id); // comparing from DB
+  if(!user){
+    return res.json({message:"user with provided ID does not exists"})
+  }
   return res.json(user);
+});
+
+
+//to post info
+app.post("/api/users", (req, res) => {
+  const body = req.body; //whatever data comes from frontend is stored here
+  users.push({ ...body, id: users.length + 1 });
+  fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
+    return res.json({ status: "success", id: users.length });
+  });
+});
+
+
+//editing using patch
+
+app.patch("/api/users/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const updatedData = req.body;
+
+  const userToUpdate = users.find((e) => e.id === id);
+  if (!userToUpdate) {
+    return res
+      .status(404)
+      .json({ Error: "User Not found, please provide a valid ID" });
+  }
+
+  Object.assign(userToUpdate, updatedData);
+  return res.json({ message: "User updated successfully", user: userToUpdate });
+});
+
+
+//for deleting user
+app.delete("/api/users/:id", (req,res)=>{
+  const id = Number( req.params.id);
+  
+
+  const userToDeleteIndex = users.findIndex((e)=>e.id === id);
+
+   //error handling
+  if(userToDeleteIndex==-1){
+    return res
+    .status(404)
+    .json({message:"No user available with that ID" ,})
+  }
+
+  // when found
+  users.splice(userToDeleteIndex,1); //users.splice(start,deletecount);
+  return res.json({message:"User deleted succesfully"})
 })
 
 
@@ -33,7 +88,4 @@ app.get("/api/users/:id", (req,res)=>{
 
 
 
-
-
-
-app.listen(PORT,()=>console.log(`Server Started at Port: ${PORT}`));
+app.listen(PORT, () => console.log(`Server Started at Port: ${PORT}`));

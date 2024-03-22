d57a5d3676c6fac558ee88a2a4b34347ca58bc26
Adarsh Singh <adarsh.singh2022@vitstudent.ac.in>
2024-03-22 10:11:46 +0530

    mongoDB installation

--- Project-01/index.js
+++ Project-01/index.js
@@ -30,7 +30,7 @@ app.use((req,res,next)=>{
 app.get("/users", (req, res) => {
   const html = `
   <ul>
-  ${users.map((e) => `<li>${e.first_name}</li>`).join("")}
+  ${users.map((e) => `<li>${e.first_name},${e.gender}</li>`).join("")}
   </ul>
   `;
   res.send(html);

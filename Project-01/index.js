f70522e1d291295989f309e9d026899d8e34b3b3
Adarsh Singh <adarsh.singh2022@vitstudent.ac.in>
2024-03-23 23:49:56 +0530

    removed data from mock

--- Project-01/index.js
+++ Project-01/index.js
@@ -1,39 +1,23 @@
 const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const fs = require("fs");
 const PORT = 8000;
-const useRouter = require("./routes/user.js")
-
-//connect
-
-mongoose.connect('mongodb://127.0.0.1:27017/db1') //returns a promise
-.then(()=>console.log("MongoDB connected"))
-.catch((err)=>console.log("Error in connection", err))
-
-
-
-
-
-
-
-
-
+const useRouter = require("./routes/user.js");
 
 //middleware:plugin
 app.use(express.urlencoded({ extended: false }));
 
-app.use((req,res,next)=>{
-console.log("Hello from Middleware 1")
-// return res.json({msg:"Hello from Middleware 1"}) //here only i stopped the req & sended back the response so user cannot access below statements.
+app.use((req, res, next) => {
+  console.log("Hello from Middleware 1");
+  // return res.json({msg:"Hello from Middleware 1"}) //here only i stopped the req & sended back the response so user cannot access below statements.
 
-//now if suppose i want to call the next fn so i will just call next();
+  //now if suppose i want to call the next fn so i will just call next();
 
-next();
-//we can also make changes to the req res objects
-//like this we use middleware
-})
+  next();
+  //we can also make changes to the req res objects
+  //like this we use middleware
+});
 
-
-app.use("/user", useRouter)
+app.use("/user", useRouter);
 app.listen(PORT, () => console.log(`Server Started at Port: ${PORT}`));

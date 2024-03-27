3e8a04a55d405910057a42ad7b6877d1050dc4ac
Adarsh Singh <adarsh.singh2022@vitstudent.ac.in>
2024-03-27 15:54:24 +0530

    factorout

--- Project-01/connection.js
+++ Project-01/connection.js
@@ -2,9 +2,9 @@ const mongoose = require("mongoose");
 
 //connect
 
-async function connectMongoDb() {
+async function connectMongoDb(url) {
  
-  return mongoose.connect("mongodb://127.0.0.1:27017/db1") //returns a promise
+  return mongoose.connect(url) //returns a promise
    
 }
 
--- Project-01/index.js
+++ Project-01/index.js
@@ -1,23 +1,19 @@
 const express = require("express");
-const app = express();
-const mongoose = require("mongoose");
-const fs = require("fs");
-const PORT = 8000;
+const{connectMongoDb} = require("./connection.js")
+const{logReqRes} = require("./middlewares")
 const useRouter = require("./routes/user.js");
+const app = express();
+const PORT = 8000;
+
+
+
+
+
+//connection
+connectMongoDb("mongodb://127.0.0.1:27017/db1");
 
 //middleware:plugin
-app.use(express.urlencoded({ extended: false }));
-
-app.use((req, res, next) => {
-  console.log("Hello from Middleware 1");
-  // return res.json({msg:"Hello from Middleware 1"}) //here only i stopped the req & sended back the response so user cannot access below statements.
-
-  //now if suppose i want to call the next fn so i will just call next();
-
-  next();
-  //we can also make changes to the req res objects
-  //like this we use middleware
-});
+app.use(logReqRes("log.txt"))
 
 app.use("/user", useRouter);
 app.listen(PORT, () => console.log(`Server Started at Port: ${PORT}`));

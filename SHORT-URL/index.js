2b5b3f75d10be8e367d2e078d84a8d8de2c1bf3d
Adarsh Singh <adarsh.singh2022@vitstudent.ac.in>
2024-06-09 15:41:33 +0530

    updateindexjs

--- SHORT-URL/index.js
+++ SHORT-URL/index.js
@@ -3,6 +3,7 @@ const app = express();
 const PORT = 7001;
 const urlRoute = require('./routes/url');
 const {connectToMongoDB} = require("./connection");
+const URL = require('./model/url')
 
 connectToMongoDB('mongodb://127.0.0.1:27017/short-url')
 .then(()=>console.log("MongoDb Connected"));
@@ -10,5 +11,18 @@ connectToMongoDB('mongodb://127.0.0.1:27017/short-url')
 app.use(express.json());
 
 app.use("/url", urlRoute);
+app.get("/shortId",async(req,res)=>{
+const shortId = req.params.shortId;
+const entry = await URL.findOneAndUpdate({
+  shortId
+},{ $push:{
+  visitHistory:{
+     timestamp:Date.now(),
+  },
+}
+})
+res.redirect(entry.redirectUrl)
+})
+
 
 app.listen(PORT,()=> console.log(`Server Started at port: ${PORT}`))

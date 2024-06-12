f3a27517b34b47ee2e2a9ec4e3063d1426827a2b
Adarsh Singh <adarsh.singh2022@vitstudent.ac.in>
2024-06-13 00:06:05 +0530

    cleaning

--- SHORT-URL/routes/url.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const {handleGenerateNewShortURl} = require("../controllers/url")
-
-
-router.post("/",handleGenerateNewShortURl );
-
-module.exports = router;
